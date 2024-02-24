25bf3ea9bd4c9c5046edde4e7c04bd8b2aec92f4
Adam Macumber <adam@macumber.io>
2024-02-24 09:12:12 -0500

    Add defered data.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from "@remix-run/node";
+import { defer, type MetaFunction } from "@remix-run/node";
+import { Await, useLoaderData } from "@remix-run/react";
+import { Suspense } from "react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +9,28 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+export const loader = () => {
+  const promise = delay(1000).then(() => {
+    return { message: "Hello from the server!" };
+  });
+
+  return defer({
+    promise,
+  });
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Await resolve={data.promise}>
+          {(data) => <p>{data.message}</p>}
+        </Await>
+      </Suspense>
     </div>
   );
 }
